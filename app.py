7ef726350f6d83679cd868d51794a2a60ad951cf
Balaji <ambalaji@outlook.com>
2025-06-25 23:29:09 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
-app = Flask(__name__, static_folder='myportfolio/static', template_folder='myportfolio/templates')
+from flask import Flask
+
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   """Homepage route."""
+   return "<html><body><h1>Hello, World!</h1></body></html>"
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+   app.run(debug=True)
